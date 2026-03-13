c146b32cd1748dd4e5ea9fe1870813ca8ec41551
zanemcmorris <zamc8851@colorado.edu>
2026-03-13 09:56:36 -0600

    Fixed memleak

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = dfd011c6a41fbb2db6c7adeddb33affd46196149
+AESD_ASSIGNMENTS_VERSION = 75885ef3d7e850ba01dae14bff0f9ca3b1d5347f
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
